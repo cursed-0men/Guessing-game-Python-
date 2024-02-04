0e388286d9b52402efb4049d3f180127530bb80a
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-02-04 18:27:49 +0530

    Create GuessGame.py

---
+++ GuessGame.py
@@ -0,0 +1,19 @@
+# Guess the number from 1-10.
+
+import random
+n = random.randint(1,10)
+
+l = 1
+h = 10
+while l <= h:
+  guess = int(input("Enter number(b/w 1-10) : "))
+  if guess < n:
+    print("Your guess is small, Enter a larger number : ")
+    l = guess + 1
+  elif guess > n:
+    print("guess is large,enter a smaller number")
+            h = guess - 1
+  else:
+            print("BINGO!!!-Correct guess")
+            break
+    
